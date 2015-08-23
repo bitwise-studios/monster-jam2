1d3491d4c10661aca3fbbb11642db6f94953a63f
Kyle Zhou <kylezhou2002@hotmail.com>
2015-08-23 15:18:59 -0700

    Some tetris stuff

---
+++ Assets/Tetris/Scripts/Spawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    [SerializeField] private GameObject[] groups;
+
+	// Use this for initialization
+	void Start () {
+        spawnNext();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void spawnNext()
+    {
+        if (Grid.grid[(int)transform.position.x, (int)transform.position.y] != null)
+        {
+            AudioSource[] allAudioSources = FindObjectsOfType<AudioSource>();
+
+            foreach (AudioSource audioS in allAudioSources)
+            {
+                audioS.Stop();
+            }
+
+            print("Game over!");
+            return;
+        }
+
+        // random index
+        int i = Random.Range(0, groups.Length);
+
+        // Spawn group at current position
+        Instantiate(groups[i], transform.position, Quaternion.identity);
+
+    }
+}
---
+++ Assets/Tetris/Scripts/Tetromino.cs
@@ -0,0 +1,162 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tetromino : MonoBehaviour {
+    // Time since last gravity tick
+    protected float lastFall = 0;
+    [SerializeField] private AudioClip spawnSound;
+
+    // Use this for initialization
+    void Start () {
+        AudioSource.PlayClipAtPoint(spawnSound, Vector3.zero);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /* TODO: replace with AI code */
+        // Move Left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            // Modify position
+            transform.position += new Vector3(-1, 0, 0);
+
+            // See if valid
+            if (isValidGridPos())
+                // It's valid. Update grid.
+                updateGrid();
+            else
+                // It's not valid. revert.
+                transform.position += new Vector3(1, 0, 0);
+        }
+
+        // Move Right
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // Modify position
+            transform.position += new Vector3(1, 0, 0);
+
+            // See if valid
+            if (isValidGridPos())
+                // It's valid. Update grid.
+                updateGrid();
+            else
+                // It's not valid. revert.
+                transform.position += new Vector3(-1, 0, 0);
+        }
+
+        // Rotate
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            rotateLeft();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            rotateRight();
+        }
+
+        // Move Downwards and Fall
+        else if (Input.GetKeyDown(KeyCode.DownArrow) ||
+                 Time.time - lastFall >= 1)
+        {
+            // Modify position
+            transform.position += new Vector3(0, -1, 0);
+
+            // See if valid
+            if (isValidGridPos())
+            {
+                // It's valid. Update grid.
+                updateGrid();
+            }
+            else
+            {
+                // It's not valid. revert.
+                transform.position += new Vector3(0, 1, 0);
+
+                // Clear filled horizontal lines
+                Grid.deleteFullRows();
+
+                // Spawn next Group
+                FindObjectOfType<Spawner>().spawnNext();
+
+                // Disable script
+                enabled = false;
+            }
+
+            lastFall = Time.time;
+        }
+    }
+
+    protected bool isValidGridPos()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.roundVec2(child.position);
+
+            // Not inside Border?
+            if (!Grid.insideBorder(v))
+                return false;
+
+            // Block in grid cell (and not part of same group)?
+            if (Grid.grid[(int)v.x, (int)v.y] != null &&
+                Grid.grid[(int)v.x, (int)v.y].parent != transform)
+                return false;
+        }
+        return true;
+    }
+
+    virtual protected bool rotateLeft()
+    {
+        transform.Rotate(0, 0, 90);
+
+        // See if valid
+        if (isValidGridPos())
+        {
+            // It's valid. Update grid.
+            updateGrid();
+            return true;
+        }
+        else
+        {
+            // It's not valid. revert.
+            transform.Rotate(0, 0, -90);
+            return false;
+        }
+    }
+
+    virtual protected bool rotateRight()
+    {
+        transform.Rotate(0, 0, -90);
+
+        // See if valid
+        if (isValidGridPos())
+        {
+            // It's valid. Update grid.
+            updateGrid();
+            return true;
+        }
+        else
+        {
+            // It's not valid. revert.
+            transform.Rotate(0, 0, 90);
+            return false;
+        }
+    }
+
+    protected void updateGrid()
+    {
+        // Remove old children from grid
+        for (int y = 0; y < Grid.h; ++y)
+            for (int x = 0; x < Grid.w; ++x)
+                if (Grid.grid[x, y] != null)
+                    if (Grid.grid[x, y].parent == transform)
+                        Grid.grid[x, y] = null;
+
+        // Add new children to grid
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Grid.roundVec2(child.position);
+            Grid.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+}
